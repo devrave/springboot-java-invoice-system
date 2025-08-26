781c82b8bfc8f4859edde70257404525adf6ff98
andres1988 <a.canorave@gmail.com>
2025-08-26 13:22:33 -0500

    setup Java Spring Boot invoice management application

---
+++ src/main/java/com/andres/springboot/di/factura/springboot_difactura/models/Item.java
@@ -0,0 +1,23 @@
+package com.andres.springboot.di.factura.springboot_difactura.models;
+
+public class Item {
+
+  private Product product;
+  private Integer quality;
+  
+  public Product getProduct() {
+    return product;
+  }
+  public void setProduct(Product product) {
+    this.product = product;
+  }
+  public Integer getQuality() {
+    return quality;
+  }
+  public void setQuality(Integer quality) {
+    this.quality = quality;
+  }
+
+  
+
+}
